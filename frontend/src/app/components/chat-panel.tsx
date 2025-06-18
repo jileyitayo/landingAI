a158de8a8328d631664948ddafb3983863a44a62
Jileyitayo <jileyitayo@gmail.com>
2025-06-18 23:01:30 +0100

    amended changes with chat panel view

--- frontend/src/app/components/chat-panel.tsx
+++ frontend/src/app/components/chat-panel.tsx
@@ -72,7 +72,7 @@ export const ChatPanel = ({ onDraftGenerated, niche }: ChatPanelProps) => {
       <CardHeader>
         <CardTitle>Landing AI</CardTitle>
       </CardHeader>
-      <CardContent className="flex-1 flex flex-col p-0 max-h-[100lvh-280px-280px]">
+      <CardContent className="flex-1 flex flex-col p-0">
         <ChatMessages messages={messages} isLoading={isLoading} />
         <ChatInput onSendMessage={handleSendMessage} isLoading={isLoading} />
       </CardContent>
